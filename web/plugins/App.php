840a194ae8dadd079245e1f3c6fc9ba765ac3899
Paolo Savoldi <paooolino@gmail.com>
2017-09-07 10:53:19 +0200

    one-to-many relationships

--- web/plugins/App.php
+++ web/plugins/App.php
@@ -41,7 +41,18 @@ class App
 				echo nl2br($fieldvalue);
 				break;			
 				
-			default:	// default is "text"
+			default:	// default is "text". may be a relation.
+				$relationlink = "";
+				$relparts = explode("_", $fieldname);
+				if (count($relparts) == 2) {
+					$extern_table = $relparts[0];
+					$extern_bean = $this->_machine->plugin("Database")->load($extern_table, $fieldvalue);
+					$namefield = $this->_getFirstTextualField($extern_table);
+					$extern_name = $extern_bean->{$namefield};
+					//$url = $this->_machine->plugin("Link")->Get("/$extern_table/$fieldvalue/");
+					//$fieldvalue = '<a href="' . $url . '" class="relationlink">' . $fieldvalue . '</a> ' . $extern_name;
+					$fieldvalue = $extern_name;
+				}
 				echo $fieldvalue;
 				break;			
 		}
@@ -67,7 +78,19 @@ class App
 				echo '<textarea class="code" name="' . $fieldname . '">' . $fieldvalue . '</textarea>';
 				break;			
 				
-			default:	// default is "text"
+			default:	// default is "text". may be a relation.
+				$relparts = explode("_", $fieldname);
+				if (count($relparts) == 2) {
+					$extern_table = $relparts[0];
+					$namefield = $this->_getFirstTextualField($extern_table);
+					$options = $this->_machine->plugin("Database")->find($extern_table, "ORDER BY $namefield", []);
+					$options_html = '';
+					foreach ($options as $option) {
+						$options_html .= '<option ' . ($fieldvalue == $option->id ? "selected" : "") . ' value="' . $option->id . '">' . $option->{$namefield} . '</option>';
+					}
+					echo '<select name="' . $fieldname . '">' . $options_html . '</select>';
+					break;
+				}
 				echo '<input ' . (($fieldname == "id") ? "disabled" : "") . ' name="' . $fieldname . '" value="' . htmlentities($fieldvalue) . '">';
 				break;			
 		}
@@ -76,6 +99,18 @@ class App
 	public function getConfig()
 	{
 		return $this->_config;
+	}
+	
+	private function _getFirstTextualField($table)
+	{
+		$db = $this->_machine->plugin("Database");
+		$fields = $db->getFields($table);
+		foreach ($fields as $f => $t) {
+			if (strpos($t, "VARCHAR") !== false) {
+				return $f;
+			}
+		}
+		return "id";
 	}
 	
 	private function _getFieldType($tablename, $fieldname)
